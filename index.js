57e5cc83fe18f0fa1333dfbfedca6871a39e1011
Eedoh <ekabelly@gmail.com>
2021-04-29 17:01:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: './env/.env' });
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const { errHandler } = require('./middlewares/middlewares');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => errHandler(err, req, res));
+
+app.listen(process.env.PORT, () => console.log(`app listening on port ${process.env.PORT}`))
